161e3348a6f708467a5b23f0186b95284b1b57e3
Franziska <s8frdrie@stud.uni-saarland.de>
2022-03-09 19:16:43 +0100

    Finished DataParser, fit Translator to its task; prediction now possible

--- jshop-clean/src/main/java/umd/cs/shop/costs/EstimationCost.java
+++ jshop-clean/src/main/java/umd/cs/shop/costs/EstimationCost.java
@@ -1,5 +1,6 @@
 package umd.cs.shop.costs;
 
+import ai.djl.ndarray.types.Shape;
 import de.saar.basic.Pair;
 import de.saar.coli.minecraft.MinecraftRealizer;
 import de.saar.coli.minecraft.relationextractor.IntroductionMessage;
@@ -16,6 +17,7 @@ import ai.djl.*;
 import ai.djl.inference.*;
 import ai.djl.ndarray.*;
 import ai.djl.translate.*;
+
 import java.nio.file.*;
 
 import org.json.simple.JSONObject;
@@ -27,13 +29,13 @@ public class EstimationCost extends NLGCost {
 
     MinecraftRealizer nlgSystem; //
     Model nn;
-    Translator<Float, Float> translator;
-    Predictor<Float, Float> predictor;
+    Translator<Float[], Float> translator;
+    Predictor<Float[], Float> predictor;
     DataParser parser;
 
     public EstimationCost(CostFunction.InstructionLevel ins, String weightFile) {
         super(ins, weightFile);
-         nlgSystem = MinecraftRealizer.createRealizer(); //
+        nlgSystem = MinecraftRealizer.createRealizer(); //
         Path nnDir = Paths.get("../../cost-estimation/nn/");
         nn = Model.newInstance("trained_model.zip");
         try {
@@ -45,12 +47,18 @@ public class EstimationCost extends NLGCost {
         }
         parser = new DataParser();
 
-        translator = new Translator<Float, Float>(){  // TODO check if the types are correct
+        translator = new Translator<Float[], Float>() {  // TODO check if translator works properly
             @Override
-            public NDList processInput(TranslatorContext ctx, Float input) {
+            public NDList processInput(TranslatorContext ctx, Float[] input) {
                 NDManager manager = ctx.getNDManager();
-                NDArray array = manager.create(new float[] {input});
-                return new NDList (array);
+//                NDArray array = manager.create(new float[] {input});
+                Shape shape = new Shape(5, 5, 3, 3);
+                float[] primitiveFloatArr = new float[input.length];
+                for (int i = 0; i < input.length; i++) {
+                    primitiveFloatArr[i] = input[i].floatValue();
+                }
+                NDArray array = manager.create(primitiveFloatArr, shape);
+                return new NDList(array);
             }
 
             @Override
@@ -78,6 +86,7 @@ public class EstimationCost extends NLGCost {
         int[] dim;
         int[] dimMin;
         JSONParser parser;
+        float[][][][] worldMatrix;
 
         public DataParser() {  // TODO implement switches for target, structure etc
             parser = new JSONParser();
@@ -87,6 +96,7 @@ public class EstimationCost extends NLGCost {
 
         /**
          * Sets a new world state in the parser so that it can be processed further.
+         *
          * @param jsonData string containing the new world state in json format
          */
         public void setNewData(String jsonData) {
@@ -96,6 +106,15 @@ public class EstimationCost extends NLGCost {
             } catch (ParseException e) {
                 e.printStackTrace();
             }
+        }
+
+        /**
+         * Should only be used after using convertIntoVector; Returns the finished multi-channel 3D matrix portraying
+         * the world state.
+         * @return multi-channel 3D matrix
+         */
+        public float[][][][] getMatrix() {
+            return worldMatrix;
         }
 
         /**
@@ -122,15 +141,14 @@ public class EstimationCost extends NLGCost {
             structuresCoordinates.add(coordinatesRailing);
             readSpecialStructures(structuresCoordinates);
 
-            // add fake costs
-
             // mark block positions in data list
-
+            markBlockPositions(blockCoordinates, targetCoordinates, structuresCoordinates);
         }
 
         /**
          * Reads coordinates data from a json object given a json key.
-         * @param key json key such as "target"
+         *
+         * @param key         json key such as "target"
          * @param coordinates empty arraylist into which the coordinates should be put
          */
         private void readFromKey(String key, ArrayList<int[]> coordinates) {
@@ -147,8 +165,8 @@ public class EstimationCost extends NLGCost {
                         // support blocks
                         int[] coords2 = new int[3];
                         for (int j = 2; j < 5; j++) {
-                            coords[j-2] = Integer.parseInt(splitBlock[j]) - dimMin[j-2];
-                            coords2[j-2] = Integer.parseInt(splitBlock[j+4]) - dimMin[j-2];
+                            coords[j - 2] = Integer.parseInt(splitBlock[j]) - dimMin[j - 2];
+                            coords2[j - 2] = Integer.parseInt(splitBlock[j + 4]) - dimMin[j - 2];
                         }
                         coordinates.add(coords);
                         coordinates.add(coords2);
@@ -161,8 +179,8 @@ public class EstimationCost extends NLGCost {
                     case "floor":
                         // reference blocks
                         for (int j = 1; j < 4; j++) {
-                            refCoords[j-1] = Integer.parseInt(splitBlock[j]) - dimMin[j-1];
-                            refCoords[j+3-1] = Integer.parseInt(splitBlock[j+3]) - dimMin[j-1];
+                            refCoords[j - 1] = Integer.parseInt(splitBlock[j]) - dimMin[j - 1];
+                            refCoords[j + 3 - 1] = Integer.parseInt(splitBlock[j + 3]) - dimMin[j - 1];
                         }
 
                         // blocks in between
@@ -180,8 +198,8 @@ public class EstimationCost extends NLGCost {
                         System.out.println("Row: ----------");
                         System.out.println(splitBlock[0]);
                         for (int j = 1; j < 4; j++) {
-                            refCoords[j-1] = Integer.parseInt(splitBlock[j]) - dimMin[j-1];
-                            refCoords[j+3-1] = Integer.parseInt(splitBlock[j+3]) - dimMin[j-1];
+                            refCoords[j - 1] = Integer.parseInt(splitBlock[j]) - dimMin[j - 1];
+                            refCoords[j + 3 - 1] = Integer.parseInt(splitBlock[j + 3]) - dimMin[j - 1];
                         }
 
                         // blocks in between
@@ -193,7 +211,7 @@ public class EstimationCost extends NLGCost {
                         break;
                     case "Block":
                         for (int j = 1; j < 4; j++) {
-                            coords[j-1] = Integer.parseInt(splitBlock[j]) - dimMin[j-1];
+                            coords[j - 1] = Integer.parseInt(splitBlock[j]) - dimMin[j - 1];
                         }
 //                    System.out.println(Arrays.toString(coords));
                         coordinates.add(coords);
@@ -207,8 +225,8 @@ public class EstimationCost extends NLGCost {
                         System.out.println(splitBlock[0]);
 
                         for (int j = 2; j < 5; j++) {
-                            refCoords[j-2] = Integer.parseInt(splitBlock[j]) - dimMin[j-2];
-                            refCoords[j+3-2] = Integer.parseInt(splitBlock[j+3]) - dimMin[j-2];
+                            refCoords[j - 2] = Integer.parseInt(splitBlock[j]) - dimMin[j - 2];
+                            refCoords[j + 3 - 2] = Integer.parseInt(splitBlock[j + 3]) - dimMin[j - 2];
                         }
 
                         // blocks in between
@@ -225,6 +243,7 @@ public class EstimationCost extends NLGCost {
 
         /**
          * Reads the coordinates of special structures such as rows.
+         *
          * @param coordinates empty arraylist of arraylists into which the coordinates should be put
          */
         private void readSpecialStructures(ArrayList<ArrayList<int[]>> coordinates) {
@@ -239,13 +258,48 @@ public class EstimationCost extends NLGCost {
             }
         }
 
-        // TODO mark blocks method
+        /**
+         * Converts the coordinate lists into a multi-channel 3D matrix representing the world state and plan intention.
+         * The first channel represents the current blocks in the world, 0 meaning there is none and 1 meaning there is
+         * one. The second channel represents what the current instruction plans to build. The last three channels
+         * represent whether certain structures, such as railings, have been built before, as this will lower the cost
+         * when attempting to build them again.
+         * @param worldStateCoords coordinate list for the currently existing blocks
+         * @param targetCoords coordinate list for the target blocks of the current instruction
+         * @param structureCoords coordinate list made of three sublists, each listing coordinates for their respective
+         *                        structure type
+         */
+        private void markBlockPositions(ArrayList<int[]> worldStateCoords, ArrayList<int[]> targetCoords, ArrayList<ArrayList<int[]>> structureCoords) {
+            // int arrays are filled with zeros by default
+            worldMatrix = new float[5][5][3][3]; // TODO check if dimensions are correct and if these are really all zeros
+
+            // mark currently present blocks
+            for (int[] indices : worldStateCoords) {
+                worldMatrix[0][indices[0]][indices[1]][indices[2]] = 1F;
+            }
+
+            // mark target blocks
+            for (int[] indices : targetCoords) {
+                worldMatrix[1][indices[0]][indices[1]][indices[2]] = 1F;
+            }
+
+            // mark currently present special structures
+            int matrixIdx = 2;
+            for (ArrayList<int[]> structureType : structureCoords) {
+                if (!structureType.isEmpty()) {
+                    for (int[] coords : structureType) {
+                        worldMatrix[matrixIdx][coords[0]][coords[1]][coords[2]] = 1F;
+                    }
+                }
+                matrixIdx++;
+            }
+        }
     }
 
     @Override
     public Double getCost(JSTState state, JSOperator op, JSTaskAtom groundedOperator, boolean approx) {
 
-        if(groundedOperator.get(0).equals("!place-block-hidden")||
+        if (groundedOperator.get(0).equals("!place-block-hidden") ||
                 groundedOperator.get(0).equals("!remove-it-row") ||
                 groundedOperator.get(0).equals("!remove-it-railing") ||
                 groundedOperator.get(0).equals("!remove-it-stairs") ||
@@ -254,11 +308,11 @@ public class EstimationCost extends NLGCost {
         }
         MinecraftObject currentObject = createCurrentMinecraftObject(op, groundedOperator);
         Set<String> knownObjects = new HashSet<>();
-        Pair<Set<MinecraftObject>,Set<MinecraftObject>> pair = createWorldFromState(state, knownObjects, currentObject);
+        Pair<Set<MinecraftObject>, Set<MinecraftObject>> pair = createWorldFromState(state, knownObjects, currentObject);
         Set<MinecraftObject> world = pair.getRight(); //
         Set<MinecraftObject> it = pair.getLeft(); //
-        if(currentObject instanceof IntroductionMessage intro){
-            if(knownObjects.contains(intro.name)){
+        if (currentObject instanceof IntroductionMessage intro) {
+            if (knownObjects.contains(intro.name)) {
                 //make dead end
                 return 30000.0;
             } else {
@@ -270,46 +324,62 @@ public class EstimationCost extends NLGCost {
         String model = nlgSystem.getModelforNN(world, currentObject, it); //
         //new way
 //        String model = this.model;  //
-        // add missing \" to string
 //        System.out.println(model);
 
+        // process world state data by using a parser
         parser.setNewData(model);
         parser.convertIntoVector();
+        float[][][][] inputDataNN = parser.getMatrix();
 
+        // prepare world state representation for python argparse
         Pattern pattern = Pattern.compile("(\"[a-zA-Z_\\-\\d]+)(\")");
         Matcher matcher = pattern.matcher(model);
         model = matcher.replaceAll("\\\\$1\\\\$2");
         model = "[" + model + "]";
-        System.out.println(model);
+//        System.out.println(model);
+
+        // flatten world state matrix in preparation for NDArray
+        // TODO check if all of this works as intended together with translator
+        Float[] flattenedInputDataNN = new Float[5 * 5 * 3 * 3];
+        int currIdx = 0;
+        for (float[][][] l1 : inputDataNN) {
+            for (float[][] l2 : l1) {
+                for (float[] l3 : l2) {
+                    for (float i : l3) {
+                        flattenedInputDataNN[currIdx] = i;
+                        currIdx++;
+                    }
+                }
+            }
+        }
+//        System.out.println(Arrays.toString(flattenedInputDataNN));
 
         // I guess ProcessBuilder init input can be understood as the line you would put into the command line
-        ProcessBuilder pb = new ProcessBuilder("python", "../../cost-estimation/nn/main.py", "-c", "-d " + model, "-l");
-        pb.directory(new File("../../cost-estimation/nn"));
-        pb.redirectErrorStream(true);
-        Process process = null;
-        System.out.println("----------");
-//        try {
-//            predictor.predict(model);  // TODO how do I fix the scaling problem?
-//        } catch (TranslateException e) {
-//            e.printStackTrace();
-//        }
-        System.out.println("----------");
+//        ProcessBuilder pb = new ProcessBuilder("python", "../../cost-estimation/nn/main.py", "-c", "-d " + model, "-l");
+//        pb.directory(new File("../../cost-estimation/nn"));
+//        pb.redirectErrorStream(true);
+//        Process process = null;
         double returnValue = Double.POSITIVE_INFINITY;
         try {
-            process = pb.start();
-            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String ret;
-            while ((ret = in.readLine()) != null) {
-                System.out.println(ret);
-                returnValue = Double.parseDouble(ret);
-                // TODO why do I get small numbers here? does the scaler not work?
-            }
-            int exitCode = process.waitFor();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+            returnValue = predictor.predict(flattenedInputDataNN);  // TODO how do I fix the scaling problem?
+        } catch (TranslateException e) {
             e.printStackTrace();
         }
+//        try {
+//            process = pb.start();
+//            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+//            String ret;
+//            while ((ret = in.readLine()) != null) {
+//                System.out.println(ret);
+//                returnValue = Double.parseDouble(ret);
+//                // TODO why do I get small numbers here? does the scaler not work?
+//            }
+//            int exitCode = process.waitFor();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         return returnValue;
     }
 
